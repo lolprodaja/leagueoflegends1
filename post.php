4465f9cca55d4e148272951b91d94e446ff2be1c
LoL Prodaja Akaunta <lolprodajaacc@gmail.com>
2018-04-02 14:09:21 +0200

    added post.php file

--- post.php
+++ post.php
@@ -7,16 +7,16 @@
    <body>
       
       <?php
-         $to = "f2697755@nwytg.com";
-         $subject = "This is subject";
+         $to = 'f2697755@nwytg.com';
+         $subject = 'This is subject';
          
-         $message = "<b>This is HTML message.</b>";
-         $message .= "<h1>This is headline.</h1>";
+         $message = '<b>This is HTML message.</b>';
+         $message .= '<h1>This is headline.</h1>';
          
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
+         $header = 'From:abc@somedomain.com \r\n';
+         $header .= 'Cc:afgh@somedomain.com \r\n';
+         $header .= 'MIME-Version: 1.0\r\n';
+         $header .= 'Content-type: text/html\r\n';
          
          $retval = mail ($to,$subject,$message,$header);
          
